328385b8aeb9c4fbbeb56cf0149a94887f3bd4fc
Aijia Yan <aijiay@berkeley.edu>
2012-12-04 16:54:17 -0800

    mrjob files added

--- counts_per_action_per_browser.py
+++ counts_per_action_per_browser.py
@@ -1,21 +1,26 @@
+'''This program returns counts per action per browser'''
+
 from mrjob.job import MRJob
 from mrjob.protocol import JSONValueProtocol
-import re
-
-actions=['page_load','save_URL','redirect']
+from itertools import izip
+from collections import defaultdict
 
 class Countsperactionbrowser(MRJob):
 	INPUT_PROTOCOL = JSONValueProtocol
 
+	'''mapper will map browser name as a key, and action type and count as a tuple'''
 	def mapper(self, _, log):
-		unique_browser = log['browser']
-		yield "browser", unique_browser
-
-	def reducer(self, key, browsers ):
-		iterable_list = list(browsers)
-		number_of_total_actions = len(iterable_list)
-		number_of_unique_browsers = len(set(iterable_list))
-		yield "counts per action per browser:", float (number_of_total_actions) / float(number_of_unique_browsers) / len(actions)
+		yield log['browser'], (log['action'],1)
+		
+	'''reducer will aggregate all the values of per action per browser into a dicionary'''
+	def reducer(self, browser, action_counts):
+		action_type, action_count = izip(*action_counts)
+		action_list = zip(action_type, action_count)
+		d1 = defaultdict(list)
+		for k,v in action_list:
+			d1[k].append(v)
+			d = dict( (k,sum(tuple(v))) for k,v in d1.iteritems() )
+		yield browser, d
 
 if __name__ == '__main__':
 	Countsperactionbrowser.run()
--- most_followed_URLs.py
+++ most_followed_URLs.py
@@ -1,3 +1,5 @@
+'''This program returns the URL that gets the most redirects'''
+
 from mrjob.job import MRJob
 from mrjob.protocol import JSONValueProtocol
 import re
@@ -7,22 +9,22 @@ actions=['page_load','save_URL','redirect']
 class MostfollowedURLs(MRJob):
 	INPUT_PROTOCOL = JSONValueProtocol
 
+	'''maps all the URL and their count'''
 	def get_urls(self, _, log):
 		if log['action'] == 'redirect':
 			yield log['url'], 1
 
-
+	'''sums the counts of each URL and maps them into the same metric'''
 	def sum_counts(self, url, counts):
 		total_of_url = sum(counts)
 		yield 'max', (total_of_url, url)
 
+	'''gets the URL that has the max counts'''
 	def get_max(self, metric, url_counts):
 		yield 'max', max(url_counts)
 
 	def steps(self):
 		return [self.mr(self.get_urls, self.sum_counts),self.mr(reducer=self.get_max)]
-
-	
 
 if __name__ == '__main__':
 	MostfollowedURLs.run()
